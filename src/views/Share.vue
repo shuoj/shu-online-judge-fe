168d7ee40da89b9c493d588f6edd44499be5e958
iris <913007768zyw@gmail.com>
2019-11-02 23:58:14 +0800

    fix: share错误信息

--- src/views/Share.vue
+++ src/views/Share.vue
@@ -6,6 +6,10 @@
         <code class="cpp"></code>
       </pre>
     </div>
+    <h2>错误信息</h2>
+    <h4>信息：{{errMsg.message}}</h4>
+    <h4>错误：{{errMsg.error}}</h4>
+    <h4>结果：{{errMsg.result}}</h4>
   </div>
 </template>
 
@@ -17,12 +21,14 @@ import api from '../api/api';
 export default class Share extends Vue {
   sourceCode: string = '';
   title: string = '';
+  errMsg: any = {};
 
   mounted() {
     const params = this.$route.params;
     api.getCommit({ id: params.id }).then((res: any) => {
       this.sourceCode = res.data.code.replace(/\\n/g, '\n');
       this.title = res.data.problemTitle;
+      this.errMsg = JSON.parse(res.data.resultDetail);
     }).catch((err: any) => {
       console.log(err);
     });
@@ -37,6 +43,15 @@ export default class Share extends Vue {
       margin: 0 auto;
       padding-top: 24px;
     }
+}
+h2 {
+  font-weight: 500;
+  font-size: 36px;
+  text-align: left;
+  margin: 12px auto 18px 0;
+}
+h4 {
+  margin: 10px auto 10px 0;
 }
 .container {
   text-align: left;
